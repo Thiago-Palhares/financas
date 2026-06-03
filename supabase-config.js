01a47ec9ddf0390852517184206825de111a6d9b
Thiago <thiago_palhares@outlook.com>
2026-06-03 11:43:11 -0300

    configura supabase em producao

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,5 @@
 window.FINANCAS_AI_SUPABASE = {
-  url: "",
-  anonKey: "",
+  url: "https://upvwalmlgesrlbqhyjlu.supabase.co",
+  anonKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVwdndhbG1sZ2VzcmxicWh5amx1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0ODk1NjEsImV4cCI6MjA5NjA2NTU2MX0.AN76BTejHhe0R7rJQtgvJcGoOtEXIbyYq4Vhbq1eEtg",
 };
